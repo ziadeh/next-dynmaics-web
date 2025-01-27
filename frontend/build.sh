6f20d2f01c17bd78d16baf77a7c5e86478b74882
Ziad Ziadeh <ziadeh50@gmailcom>
2025-01-27 15:18:12 +0200

    Fixes

--- frontend/build.sh
+++ frontend/build.sh
@@ -6,5 +6,5 @@ cd .next
 s3cmd put --acl-public --recursive --guess-mime-type --no-mime-magic --exclude "*.css" --exclude "*.js" static/ "$BUCKET_PATH"
 s3cmd put --acl-public --recursive -m "text/css" --exclude "*" --include "*.css" static/ "$BUCKET_PATH"
 s3cmd put --acl-public --recursive -m "application/javascript" --exclude "*" --include "*.js" static/ "$BUCKET_PATH"
-pm2 restart 1
+pm2 restart 0
 echo "DONE"
