95a53166c3223b2fce71b871e6d0d106e7d244e8
Ziad Ziadeh <ziadeh50@gmailcom>
2025-01-28 10:52:39 +0200

    Update menu

--- frontend/src/components/shared/SiteMenu.tsx
+++ frontend/src/components/shared/SiteMenu.tsx
@@ -124,7 +124,7 @@ function SiteMenu({ menu }: { menu: Menu }) {
           <NavigationMenuItem>
             <Link href="/contact-us" legacyBehavior passHref>
               <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
-                Contact
+                Contact Us
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
