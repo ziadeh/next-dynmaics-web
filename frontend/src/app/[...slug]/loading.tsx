5797410e4ac18143b63dc896c92b7e464b0e2ac0
Ziad Ziadeh <ziadeh50@gmailcom>
2025-08-22 02:25:50 +0300

    Added video to Spaces

--- frontend/src/app/[...slug]/loading.tsx
+++ frontend/src/app/[...slug]/loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Loading() {
-  return <div className="bg-nd-secondary-950">Loading...</div>;
+  return <div className="fixed h-screen w-screen bg-nd-secondary-950"></div>;
 }
